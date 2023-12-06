52f486ed90c83504660930b5d4949309a43e2f42
Brent Liu <brentliu02@gmail.com>
2023-12-06 12:24:37 -0500

    added pycache to gitignore. build_train_test now ignores Approach3, will redo this approach in future. train dataset is now split into different files by approach

--- build_train_test.py
+++ build_train_test.py
@@ -231,13 +231,12 @@ if __name__ == "__main__":
     num_data = len(all_data)
     train_data, test_data = train_test_split(all_data, test_size=0.3, random_state=0)
 
-    approaches = [Approach1, Approach2, Approach3]
+    approaches = [Approach1, Approach2]
 
-    train_dataset = []
     for approach in approaches:
-        train_dataset += approach.build_train_dataset(train_data)
-    with jsonlines.open('train_dataset.jsonl', mode='w') as writer:
-        writer.write_all(train_dataset)
+        train_dataset = approach.build_train_dataset(train_data)
+        with jsonlines.open(f'{approach.__name__}_train_dataset.jsonl', mode='w') as writer:
+            writer.write_all(train_dataset)
 
     for approach in approaches:
         test_dataset = approach.build_test_dataset(test_data)
